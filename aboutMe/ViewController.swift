d9c9f12064bda4c8c042b6b3f910ffe5d6e47d61
Vadim Tatarchuk <tatarchuk.v.b@gmail.com>
2020-10-26 11:15:29 +0500

    Initial Commit

---
+++ aboutMe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  aboutMe
+//
+//  Created by  Vadim Tatarchuk on 26.10.2020.
+//  Copyright © 2020  Vadim Tatarchuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
